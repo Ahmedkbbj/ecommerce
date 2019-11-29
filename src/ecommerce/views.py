94bb400b9d59580f51a24ae9dfc34fc50b65af96
Ahmed_kbbj <ahmed.kab20@gmail.com>
2019-11-29 18:45:06 +0100

    create latest_products in home

--- src/ecommerce/views.py
+++ src/ecommerce/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
-
+from .models import Product
 # Create your views here.
 
 def homeview(request):
+
+    lates_products = Product.objects.all().order_by('created_at')
+    lates_products = lates_products[2] if lates_products.exists() else []
+
+    products = Product.objects.exclude(pk__in=[item.pk for item in lates_products]) 
+    products = products[7] if products.exists() else Product.objects.all()[7]
+
+    
+
+
     return render(request, "pages/home.html", {})
