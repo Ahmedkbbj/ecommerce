ad701b03eb78367db28c4945e967247f55c0d176
Ahmed_kbbj <ahmed.kab20@gmail.com>
2019-11-29 18:17:36 +0100

    add page home and static files and create models

--- src/ecommerce/models.py
+++ src/ecommerce/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
+GENDER = (
+    ('W','W'),
+    ('M','M')
+)
+
 # Create your models here.
+class Client(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField()
+    phone = models.CharField(max_length=15)
+    adress = models.CharField(max_length=255)
+    city = models.CharField(max_length=50)
+    age = models.PositiveIntegerField()
+    gender = models.CharField(max_length=1, choices=GENDER)
+
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=15)
+    description = models.TextField()
+    created_at = models.DateField()
+
+
+
+class Galery(models.Model):
+    image = models.ImageField()
+    product = models.ForeignKey(Product,on_delete=models.SET_NULL, null=True)
+
+
+class Command(models.Model):
+
+    client = models.ForeignKey(Client, on_delete=models.SET_NULL, null=True)
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    created_at = models.DateField()
--- src/src/settings.py
+++ src/src/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'ufi7aws72!ulm@(1wp=pe(u_*nzudhd697fozska)%b8ykl=y$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'ecommerce'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'src.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,3 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    '/var/www/static/',
+]
