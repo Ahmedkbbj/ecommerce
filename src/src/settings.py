9989ca0c985c27063cc29998d063da6f32a4fd2d
Ahmed_kbbj <ahmed.kab20@gmail.com>
2019-12-06 17:19:48 +0100

    other change in settings

--- src/src/settings.py
+++ src/src/settings.py
@@ -140,13 +140,12 @@ MEDIA_URL = '/media/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-EMAIL_HOST:  'localhost'
-EMAIL_PORT: 25
-EMAIL_HOST_USER: ''
-EMAIL_HOST_PASSWORD: ''
-EMAIL_USE_TLS: False
-EMAIL_USE_SSL: False
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST =  'localhost'
+EMAIL_PORT = 25
+# EMAIL_HOST_USER = ''
+# EMAIL_HOST_PASSWORD = ''
+# EMAIL_USE_TLS= False
+# EMAIL_USE_SSL= False
 
 
